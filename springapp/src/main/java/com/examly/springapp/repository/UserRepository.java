64ad5154ff9fafffe737f920cc24c447e3f71748
sattish03 <58936999+sattish03@users.noreply.github.com>
2022-04-12 18:29:22 +0530

    Adding User Repository

---
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+     User findByEmail(String email);
+
+     User findByUserId(long id);
+
+     @Modifying
+     @Transactional
+     @Query("")
+     editUser(long id);
+
+
+}
