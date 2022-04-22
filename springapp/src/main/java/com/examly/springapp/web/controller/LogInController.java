0917269372b7fa3be98b5be43e200114a6cfd42c
Dhananjay Singh Chauhan <97722758+ProjectB1JR37@users.noreply.github.com>
2022-04-22 18:56:10 +0530

    Adding Valid Annotation

--- springapp/src/main/java/com/examly/springapp/web/controller/LogInController.java
+++ springapp/src/main/java/com/examly/springapp/web/controller/LogInController.java
@@ -4,6 +4,7 @@ import com.examly.springapp.service.UserService;
 import com.examly.springapp.web.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.HashMap;
 
@@ -15,14 +16,14 @@ public class LogInController {
 
     @PostMapping(Constants.ADMIN_LOGIN_URL_MAPPING)
     @ResponseBody
-    public HashMap<String, String> adminLogin(@RequestBody UserRegistrationDto dto){
+    public HashMap<String, String> adminLogin(@Valid @RequestBody UserRegistrationDto dto){
 
         return userService.loginCheck(dto) ;
     }
 
     @PostMapping(Constants.STUDENT_LOGIN_URL_MAPPING)
     @ResponseBody
-    public HashMap<String, String> userLogin(@RequestBody UserRegistrationDto dto){
+    public HashMap<String, String> userLogin(@Valid @RequestBody UserRegistrationDto dto){
         return userService.loginCheck(dto);
     }
 }
